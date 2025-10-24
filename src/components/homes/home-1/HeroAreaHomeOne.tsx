cf86ec81a62f34c429b92ba7ca81061c18fe51b3
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2025-10-24 09:58:20 +0100

    Updated vital sync

--- src/components/homes/home-1/HeroAreaHomeOne.tsx
+++ src/components/homes/home-1/HeroAreaHomeOne.tsx
@@ -8,7 +8,7 @@ const HeroAreaHomeOne = () => {
         <div className="container">
           <div className="uxora-hero-content" data-anime="onview: -100; targets: >*; translateY: [48, 0]; opacity: [0, 1]; easing: spring(1, 80, 10, 0); duration: 450; delay: anime.stagger(100, {start: 200});">
             <h1 className="title tw:md:text-8xl!">Empower your blood bank management</h1>
-            <p>Vital Sync revolutionizes blood donation and bank management, ensuring efficiency and safety in every step. Join us in making a differnce in the world of blood donation.</p>
+            <p>Vital Sync revolutionizes blood donation and bank management, ensuring efficiency and safety in every step.</p>
             <div className="mt-50 hero-btn-wrap">
               <a className="uxora-default-btn hero-btn" target="__blank" href={ENV.APP_URL}>Get Started Now</a>
               <a className="uxora-default-btn hero-btn outline-btn" href="#about">Learn More</a>
