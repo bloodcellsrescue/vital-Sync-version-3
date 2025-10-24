e4d06e963b669bea406fb702691f9ffc95095543
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2025-10-24 09:56:47 +0100

    Updated vital sync

--- src/components/homes/home-1/partials/service-item.tsx
+++ src/components/homes/home-1/partials/service-item.tsx
@@ -11,7 +11,7 @@ interface ServiceItemProps {
 export const ServiceItem = ({ title, description, image, link = ENV.APP_REGISTER }: ServiceItemProps) => {
 	return (
 		<div className="uxora-content01 rounded-3 tw:mb-5 p-3 tw:shadow-lg! bg-white tw:border-gray-100!" >
-			<div className="tw:grid tw:md:grid-cols-2 gap-5" >
+			<div className="tw:grid tw:md:grid-cols-2 md:gap-5!" >
 				<div className="uxora-about-us-thumb">
 					<img className="w-100 rounded-3 tw:h-80! tw:object-cover" src={image} alt="" />
 				</div>
